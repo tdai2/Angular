331bb0b9465619416eb7f5bda8734f2827e5be73
ticktockdai <ticktock.dai@gmail.com>
2017-06-22 14:18:16 +0800

    5.5 sub route complete

--- src/app/playground/playground-routing.module.ts
+++ src/app/playground/playground-routing.module.ts
@@ -1,9 +1,30 @@
 import {Routes, RouterModule} from '@angular/router';
 import {NgModule} from '@angular/core';
-import {PlaygroundComponent} from './playground.component'
+import {PlaygroundComponent} from './playground.component';
+import {OneComponent} from './one/one.component';
+import {TwoComponent} from './two/two.component';
+import {ThreeComponent} from './one/three/three.component';
+
 const routes: Routes = [
     {path:'', 
-    component:PlaygroundComponent},
+    component:PlaygroundComponent,
+    children: [
+        {
+            path:'one',
+            component: OneComponent, 
+            children:[{
+                path:'three',
+                component: ThreeComponent,
+            }]
+        },
+        {
+            path:'two',
+            component: TwoComponent,
+        }
+
+    ]
+},
+
 ];
 
 @NgModule({
